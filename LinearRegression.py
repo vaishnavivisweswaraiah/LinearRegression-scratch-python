fc6b05175453366852bde6df25482be68c2939ce
Vaishnavi <vaishnavisharma9908@gmail.com>
2019-11-24 15:24:48 -0500

    Update LinearRegression.py

--- LinearRegression.py
+++ LinearRegression.py
@@ -43,12 +43,6 @@ class LinearRegression:
                break
            else:
                convergence=False
-           #print("hjypothesis",self.hypothesis_matrix)
-           #print("expected",self.expected)
-           #self.expected=np.nanmean(self.expected,axis=0)
-           #print("mean",self.expected)
-           #print("cost ,matrix",error_cost_matrix)
-           #print("updated iteration",i, updated_theta)
 
         return self.thetas,self.hypothesis_matrix
 
@@ -81,9 +75,7 @@ class performance_metrics:
         {\displaystyle SS_{\text{reg}}=\sum _{i}({\hat {Y}}_{i}-{\bar {Y}})^{2}}SS_{\text{reg}}=\sum _{i}({\hat {Y}}_{i}-{\bar {Y}})^{2}
         {\displaystyle SS_{\text{tot}}=\sum _{i}(Y_{i}-{\bar {Y}})^{2}}SS_{\text{tot}}=\sum _{i}(Y_{i}-{\bar {Y}})^{2}
         '''
-        #print(predicted.shape)
-        #print(expected.shape)
-        #SS_regression=np.sum(np.square(np.subtract(np.array(predicted),np.array(expected).mean())))
+        
         SS_residual = np.sum(np.square(np.subtract(np.array(expected), np.array(predicted))))
         SS_total=np.sum(np.square(np.subtract(np.array(expected),np.array(expected).mean())))
 
@@ -174,138 +166,11 @@ if __name__ == "__main__":
     print("r pearson coefficient_algorithm:",r)
     #print(np.array(data_y),np.array(pred_y))
     print("scipy pearson coefficient      :",pearsonr(test_data_y, pred_y_tool)[0])
-    #test_data_y=[1,2,3,4]
-    #pred_y=[1.2,2.4,3.4,5.4]
-    #print(pearsonr(data_y,pred_y))
-    #r = metrics.pearson_corelation_coefficient(data_y, pred_y)
-    #print(r)
     'R-squared value'
     R_squared=metrics.Rsquared_valued(test_data_y,pred_y)
     print("R-sqared algorithm :",R_squared)
     print("R-squared sklearn :",r2_score(test_data_y,pred_y_tool))
 
-    mean1 = mean_squared_error_algo
-    vr1 = r
-    VR1 = R_squared
+    
 
-    'Question - 2 '
-    print("question 2*******************************")
-    #train_set.insert(0, 'x0', 1)
-    train_data_x = train_set[['x0', 'all_mcqs_avg_n20','all_NBME_avg_n4']]
-    train_data_y = train_set['STEP_1']
-    train_data_y = np.array(train_data_y.fillna(mean)).reshape(-1, 1)
-    'test data'
-    # test_data = test_set.drop(['LEVEL'], axis=1)
-    # test_data = (test_set - test_set.mean()) / test_set.std()
-    #test_set.insert(0, 'x0', 1)
-    test_data_x = test_set[['x0', 'all_mcqs_avg_n20','all_NBME_avg_n4']]
-    test_data_y = test_set['STEP_1']
-    test_data_y = np.array(test_data_y.fillna(mean)).reshape(-1, 1)
-
-    'Linear regression function model optimization for training data'
-    Linear_obj = LinearRegression(train_data_x, train_data_y)
-    Linear_obj.initial_coefficient()
-    coeff, hyp = Linear_obj.fit_linearregression(0.1, 1000)
-
-    'prdicted value on train data'
-    train_pred_y = np.array(train_data_x["all_NBME_avg_n4"] * coeff[2]+train_data_x["all_mcqs_avg_n20"] * coeff[1] + coeff[0]).reshape(-1, 1)
-    print("algorithm coefficients", coeff[2],coeff[1], coeff[0])
-
-    'from sklearn library'
-    lr = _lr().fit(train_data_x, train_data_y)
-    # print(lr.intercept_)
-    print("sklearn coefficients",lr.coef_[0][2], lr.coef_[0][1], lr.intercept_)
-
-    'Residual plot'
-    plt.scatter(train_data_y, train_data_y - train_pred_y)
-    plt.axhline(y=0, color='r', linestyle='--')
-    plt.xlabel("predicted/fit values ")
-    plt.ylabel("residual ( observed - predicted)")
-    plt.title("Residual vs fit plot with additional feature")
-    plt.show()
-
-    'prediction on test data'
-    # print(coeff)
-    pred_y = Linear_obj.predit(test_data_x, coeff)
-    pred_y_tool = lr.predict(test_data_x)
-    # print(pred_y,pred_y_tool)
-
-    '''metrics'''
-    metrics = performance_metrics()
-    mean_squared_error_algo = metrics.Mean_Squared_error(test_data_y, pred_y)
-    print("meanerror_algorithm 2 variables", mean_squared_error_algo)
-    mean = mean_squared_error(test_data_y, pred_y_tool)
-    print("sklearn 2 variables", mean)
-    'pearson coefficient'
-    r = metrics.pearson_corelation_coefficient(test_data_y, pred_y)
-    print("r pearson coefficient_algorithm 2 variables:", r)
-    # print(np.array(data_y),np.array(pred_y))
-    print("scipy pearson coefficient 2 variables :", pearsonr(test_data_y, pred_y_tool)[0])
-    # test_data_y=[1,2,3,4]
-    # pred_y=[1.2,2.4,3.4,5.4]
-    # print(pearsonr(data_y,pred_y))
-    # r = metrics.pearson_corelation_coefficient(data_y, pred_y)
-    # print(r)
-    'R-squared value'
-    R_squared = metrics.Rsquared_valued(test_data_y, pred_y)
-    print("R-sqared algorithm 2 variables:", R_squared)
-    print("R-squared sklearn 2 variables :", r2_score(test_data_y, pred_y_tool))
-
-    mean2=mean_squared_error_algo
-    vr2=r
-    VR2=R_squared
-
-    data_type = ("MSE_onefeature",  "MSE_with_all_NBME_avg_n4’", "r_onefeature", "r_with _all_NBME_avg_n4’", "R2_onefeature","R2_with_all_NBME_avg_n4’")
-    data_type_length = np.arange(len(data_type))
-    Metrics = (mean1,mean2,vr1,vr2,VR1,VR2)
-    plt.bar(data_type_length, Metrics, align="center", alpha=0.9,color='green')
-    plt.xticks(data_type_length, data_type)
-    plt.ylabel('Metrics')
-    plt.title("Model performance with one feature and addition of all_NBME_avg_n4 ")
-    plt.show()
-    # 'Question 2'
-    # print(data.describe())
-    # #data.insert(0, 'x0', 1)
-    # data_x = data[['x0', 'all_mcqs_avg_n20','all_NBME_avg_n4']]
-    # data_y = data['STEP_1']
-    #
-    # Linear_obj = LinearRegression(data_x, data_y)
-    # Linear_obj.initial_coefficient()
-    # coeff, hyp = Linear_obj.fit_linearregression(0.1, 1000)
-    #
-    # pred_y = np.array(data_x["all_NBME_avg_n4"] * coeff[2]+data_x["all_mcqs_avg_n20"] * coeff[1] + coeff[0]).reshape(-1, 1)
-    # # print(data_y,pred_y)
-    # # print(data_x)
-    # print("algorithm coefficients", coeff[2],coeff[1], coeff[0])
-    #
-    # 'from sklearn library'
-    # lr = _lr().fit(data_x, data_y)
-    # #print(lr.coef_)
-    # print("sklearn coefficients",lr.coef_[0][2], lr.coef_[0][1], lr.intercept_)
-    #
-    #
-    # '''metrics'''
-    # metrics = performance_metrics()
-    # # data_y=[1,2,3,4]
-    # # pred_y=[1.2,2.4,3.4,5.4]
-    # # print(np.array(data_y).shape,np.array(pred_y).shape)
-    # # pred_y=np.array(pred_y).reshape((-1,1))
-    # # print(pred_y.shape)
-    # mean_squared_error_algo = metrics.Mean_Squared_error(data_y, pred_y)
-    # print("meanerror_algorithm", mean_squared_error_algo)
-    # mean = mean_squared_error(data_y, pred_y)
-    # print("sklearn", mean)
-    # 'pearson coefficient'
-    # r = metrics.pearson_corelation_coefficient(data_y, pred_y)
-    # print("r pearson coefficient_algorithm:", r)
-    # # print(np.array(data_y),np.array(pred_y))
-    # print("scipy pearson coefficient      :", pearsonr(data_y, pred_y)[0])
-    # # data_y=[1,2,3,4]
-    # # pred_y=[1.2,2.4,3.4,5.4]
-    # # print(pearsonr(data_y,pred_y))
-    # # r = metrics.pearson_corelation_coefficient(data_y, pred_y)
-    # # print(r)
-    # 'R-squared value'
-    # R_squared = metrics.Rsquared_valued(data_y, pred_y)
-    # print("Rsqared algorithm :", R_squared)
-    # print("R-squared sklearn :", r2_score(data_y, pred_y))
+ 
